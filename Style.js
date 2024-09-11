d34b33a24aa65a777c93b4f88d790fe71d85da2c
ANKITA7722 <ANKITA7722>
2024-09-11 14:36:39 +0100

    jQuery Hide element function use

---
+++ Style.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // $("button").click(function(){
+    //     $("p").hide()
+    // })
+
+    // $("button").dblclick(function(){
+    //     $("#test").hide()
+    // })
+
+    $("button").dblclick(function(){
+        $("p.new").hide()
+    })
+})
