1555cacfb5f8c5898a236dd648c88e99d410747e
ANKITA7722 <ANKITA7722>
2024-09-13 16:07:58 +0100

    task by rajesh sir

---
+++ style4.js
@@ -0,0 +1,17 @@
+// const Student = [
+//     {
+//         "roll":.121,
+//         "name":"ankita",
+//         "city": "sihor"
+//     },
+//     {
+//         "roll":.122,
+//         "name":"ankita",
+//         "city": "bhopal"
+//     },
+//     {
+//         "roll":.123,
+//         "name":"shailesh",
+//         "city": "ajmer"
+//     },
+// ]
