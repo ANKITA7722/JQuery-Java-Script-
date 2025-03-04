adcb7e267ed0c6c18a263df068d9bfe6543154ab
Ankita <bobdeankita24@gmail.com>
2025-03-05 00:47:42 +0530

    all done

--- style1.js
+++ style1.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    // $("#hide").click(function(){
-    //     $("p").hide(2000);
-    // });
-    // $("#show").click(function(){
-    //     $("p").show(2000);
-    // });
+    $("#hide").click(function(){
+        $("p").hide(2000);
+    });
+    $("#show").click(function(){
+        $("p").show(2000);
+    });
     $("#btn").click(function(){
         $("p").toggle()
         
     });
 
 
-//  $("#fadein").click(function(){
-//         $(".box1").fadeIn();
-//         $(".box2").fadeIn(1000);
-//         $(".box3").fadeIn(3000);
-//     });
-// });
-// $("#fadeOut").click(function(){
-//     $(".box1").fadeOut();
-//     $(".box2").fadeOut(1000);
-//     $(".box3").fadeOUT(3000);
+ $("#fadein").click(function(){
+        $(".box1").fadeIn();
+        $(".box2").fadeIn(1000);
+        $(".box3").fadeIn(3000);
+    });
+});
+$("#fadeOut").click(function(){
+    $(".box1").fadeOut();
+    $(".box2").fadeOut(1000);
+    $(".box3").fadeOUT(3000);
 
  
  $("#togglefade").click(function(){
